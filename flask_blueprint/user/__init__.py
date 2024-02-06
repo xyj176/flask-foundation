a38f3e8e55b269fc3c39258593df8a33e55277b0
xuyj <xuyj@dist.com.cn>
2024-02-06 17:32:17 +0800

    refactor:重添加注释

--- flask_blueprint/user/__init__.py
+++ flask_blueprint/user/__init__.py
@@ -14,5 +14,5 @@ from flask import Blueprint
 # 第一个参数是Blueprint的名称，第二个参数是Blueprint所在的package的名称，通常都是__name__,第三个参数是路由前缀
 user_blue = Blueprint(name='user', import_name=__name__, url_prefix='/user')
 
-# 引入view，让view里面的接口生效
+# 引入view，让view里面的接口生效（一定要放置在最后）
 from . import views
